a1df6cca109aee34e35cab037601cd8ae0701574
JuanCa97 <jjuanccarlos1997@hotmail.com>
2015-09-29 08:28:35 -0600

    initializing project

--- application.py
+++ application.py
@@ -29,3 +29,34 @@ def ingreso_productos():
     for KEY in ARTICLES:
         print KEY,":",ARTICLES[KEY]
 
+def shopping():
+	if len(ARTICLES)>0:
+	    BOXTWO=True
+	    TOTAL=0
+	    while BOXTWO==True:
+	        for KEY in ARTICLES:
+	        	print KEY,":",ARTICLES[KEY]
+	        PRODUCT=raw_input("Products that want to take?")
+	        for ELEMENT in ARTICLES:
+	            if ELEMENT==PRODUCT:
+	                print "you chose %s"%(ELEMENT)
+	                QUANTITY=input("any amounts desired product?")
+	                CANT=QUANTITY*ARTICLES[ELEMENT]
+	                TOTAL=TOTAL+CANT
+	                
+	                print "article %s: quantity %s: subtotal invoice Q.%s "%(ELEMENT, QUANTITY,TOTAL )
+	                RESPONSE=True
+	                while RESPONSE==True:
+	                    FOLLOW=raw_input("To select a different item y/n:")
+	                    if FOLLOW.lower()=="y":
+	                        BOXTWO=True
+	                        RESPONSE=False
+	                    elif FOLLOW.lower()=="n":
+	                        BOXTWO=False
+	                        RESPONSE=False
+	                        return TOTAL
+	                    else:
+	                        print "option invalidates"
+	                        RESPONSE=True
+	else:
+		print "No existing products"
