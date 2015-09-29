11492721c94e6d203e91d58af93990885e91437f
JuanCa97 <jjuanccarlos1997@hotmail.com>
2015-09-29 08:46:46 -0600

    modificated code

--- application.py
+++ application.py
@@ -1,67 +1,77 @@
 #! / usr / bin / python
 # -*- coding: utf-8 -*-
 """Register Machine"""
-import sys
 import os
+import sys
 
-ARTICLES={}
+ARTICLis={}
 TOTALONE=06
 
-def ingreso_productos():
-    BOX=True
-    while BOX==True:
+
+def insert_product():
+    limpiar()
+    BOX = True
+    while BOX == True:
         OPTION =raw_input("You want to enter a product? y/n: ")
-        try:
-            if OPTION.isalpha()==True:
-                if OPTION.lower()=="y":
+        if OPTION.isalpha()==True:
+            if OPTION.lower()=="y":
+                while True:
                     PRODUCT=raw_input("Insert a product: ")
-                    PRICE=int(raw_input("Insert the price of the product: "))
-                    ARTICLES[PRODUCT]=PRICE
-                elif OPTION.lower()=="n":
-                    BOX=False
-                else:
-                    print"unrecognized data"
-            else:
-                print"unrecognized data numeric"
-        except:
-            BOX=True
-    print "their existing articles are:"
-    for KEY in ARTICLES:
-        print KEY,":",ARTICLES[KEY]
+                    if PRODUCT.isalpha():
+                        break
+                    else:
+                        print"unrecognized data numeric"
+                        
+                while True:
+                    try:
+                        PRICE=float(raw_input("Insert the price of the product: "))
+                        ARTICLis[PRODUCT]=PRICE
+                        break
+                    except ValueError:
+                        print"unrecognized data"
+            elif OPTION.lower()=="n":
+                BOX=False
+        else:
+            print "their existing articlis  are:"
+
+    for KEY in ARTICLis:
+        print KEY,":",ARTICLis[KEY]
 
 def shopping():
-	if len(ARTICLES)>0:
-	    BOXTWO=True
-	    TOTAL=0
-	    while BOXTWO==True:
-	        for KEY in ARTICLES:
-	        	print KEY,":",ARTICLES[KEY]
-	        PRODUCT=raw_input("Products that want to take?")
-	        for ELEMENT in ARTICLES:
-	            if ELEMENT==PRODUCT:
-	                print "you chose %s"%(ELEMENT)
-	                QUANTITY=input("any amounts desired product?")
-	                CANT=QUANTITY*ARTICLES[ELEMENT]
-	                TOTAL=TOTAL+CANT
-	                
-	                print "article %s: quantity %s: subtotal invoice Q.%s "%(ELEMENT, QUANTITY,TOTAL )
-	                RESPONSE=True
-	                while RESPONSE==True:
-	                    FOLLOW=raw_input("To select a different item y/n:")
-	                    if FOLLOW.lower()=="y":
-	                        BOXTWO=True
-	                        RESPONSE=False
-	                    elif FOLLOW.lower()=="n":
-	                        BOXTWO=False
-	                        RESPONSE=False
-	                        return TOTAL
-	                    else:
-	                        print "option invalidates"
-	                        RESPONSE=True
-	else:
-		print "No existing products"
+    limpiar()
+    if len(ARTICLis)>0:
+        BOXTWO=True
+        TOTAL=0
+        while BOXTWO==True:
+            for KEY in ARTICLis:
+                print KEY,":",ARTICLis[KEY]
+            PRODUCT=raw_input("Products that want to take?")
+            for TheEMENT in ARTICLis:
+                if TheEMENT==PRODUCT:
+                    print "you chose %s"%(TheEMENT)
+                    QUANTITY=input("any amounts ofsired product?")
+                    CANT=QUANTITY*ARTICLis[TheEMENT]
+                    TOTAL=TOTAL+CANT
+                    
+                    print "article %s: quantity %s: subtotal invoice Q.%s "%(TheEMENT, QUANTITY,TOTAL )
+                    RisPONSE=True
+                    while RisPONSE==True:
+                        FOLLOW=raw_input("To Stheect a different item y/n:")
+                        if FOLLOW.lower()=="y":
+                            BOXTWO=True
+                            RisPONSE=False
+                        elif FOLLOW.lower()=="n":
+                            BOXTWO=False
+                            RisPONSE=False
+                            return TOTAL
+                        else:
+                            print "option invalidatis"
+                            RisPONSE=True
+    else:
+        print "No existing products"
 
 def BILL():
+    limpiar()
     BOXTWO=True
     while BOXTWO==True:
         print "1)Gold"
@@ -70,40 +80,40 @@ def BILL():
         CARD=raw_input("What kind of card do you have?: ")
         try:
             if CARD.isalpha()==False:
-                #ingreso de producto
+                #ingriso of producto
                 if CARD=="1":
-					print "Gold"
-					print u"The client has a 5%  discount:"
-					print "The subtotal of the invoice is Q.%s" %(TOTALONE)
-					IVA = (TOTALONE*0.12)
-					DISCOUNT = (TOTALONE*0.05)
-					ALLTOTAL = TOTALONE + IVA - DISCOUNT
-					print "should: %s"%(TOTALONE)
-					print "______________________"
-					CLient_name = raw_input("Client name:  ")
-					nit = raw_input("NIT: ")
-					CASH = input("CASH :  ")
-					CHANGE = CASH - TOTALONE
-					print "__________________________"
-					print ("Price       %.2f\t") % TOTALONE
-					print ("IVA          %.2f\t") % IVA
-					print ("Total        %.2f\t") % ALLTOTAL
-					print ("CASH     %.2f\t") % CASH
-					print "__________________________"
-					print "CHANGE:   %s"%(CHANGE)
-					break
+                    print "Gold"
+                    print u"The client has a 5%  discount:"
+                    print "The subtotal of the invoice is Q.%s" %(TOTALONE)
+                    IVA = (TOTALONE*0.12)
+                    DISCOUNT = (TOTALONE*0.05)
+                    ALLTOTAL = TOTALONE + IVA - DISCOUNT
+                    print "should: %s"%(TOTALONE)
+                    print "______________________"
+                    CLient_name = raw_input("Client name:  ")
+                    nit = raw_input("NIT: ")
+                    CASH = input("CASH :  ")
+                    CHANGE = CASH - TOTALONE
+                    print "__________________________"
+                    print ("Price       %.2f\t") % TOTALONE
+                    print ("IVA          %.2f\t") % IVA
+                    print ("Total        %.2f\t") % ALLTOTAL
+                    print ("CASH     %.2f\t") % CASH
+                    print "__________________________"
+                    print "CHANGE:   %s"%(CHANGE)
+                    break
                 #compra
                 elif CARD=="2":
                     print "Silver"
                     print "The client has a 2%  discount:"
-                    print "El subtotal de la factura esQ.%s"%(TOTALONE)
+                    print "The subtotal of the bill is Q.%s"%(TOTALONE)
                     IVA = (TOTALONE*0.12)
                     DISCOUNT = (TOTALONE*0.02)
                     ALLTOTAL = TOTALONE + IVA - DISCOUNT
-                    # aqui comienza facturación
+                    # aqui comienza billción
                     print "should: ",ALLTOTAL
                     print ("______________________")
-                    CLient_name = raw_input("Nombre Del Cliente: ")
+                    CLient_name = raw_input("Name The Cliente: ")
                     nit = raw_input("NIT: ")
                     CASH = input("CASH :  ")
                     CHANGE = CASH - ALLTOTAL
@@ -116,11 +126,11 @@ def BILL():
                     print ("CHANGE:   "),CHANGE
                     BOXTWO=False
 
-#Factura
+#bill
                 elif CARD =="3":
                     IVA = (TOTALONE*0.12)
                     ALLTOTAL = TOTALONE + IVA 
-                    #  aqui comienza facturación
+                    #  aqui comienza billción
                     print "should: ",ALLTOTAL
                     print ("______________________")
                     CLient_name = raw_input("Client name:  ")
@@ -137,47 +147,60 @@ def BILL():
                     print ("CHANGE:   "),CHANGE
                     BOXTWO=False
                 else:
-                    print "opcion no valida"
+                    print "Option invalidates"
             else:
-                print "solo se aceptan numeros"
+                print "Unrecognized data numeric"
         except:
             opcion3=True
-	print"Gracias por su compra, Regrese Pronto."
+    print"Thank you for your purchase, come back soon."
 
 
+def limpiar():
+    os.system("reset")
+
+
+def salir():
+    sys.exit()
 #menú
+
 salir=False
 while salir==False:
-    print "Caja Registradora"
-    print "¿Qué desea realizar?"
-    print "1.) Ingreso productos"
-    print "2.) Compras"
-    print "3.) factura"
-    opmenu = raw_input("ingrese número de Menu: ")
+    print "Cash Register"
+    print "¿What would you do?"
+    print "1.) Insert_product"
+    print "2.) Shopping"
+    print "3.) Bill"
+    print "4.) Exit"
+    opmenu = raw_input("insert numeric of Menu: ")
     try:
-	    if opmenu.isalpha()==False:
-		    if opmenu =="1":
-		        ingreso_productos()
-		        opcionmenu=raw_input("Desea volver al menu SI/NO: ")
-		        if opcionmenu.lower()=="si":
-		        	salir=False
-		        else:
-		            break
-		    elif opmenu =="2":
+        if opmenu.isalpha()==False:
+            if opmenu =="1":
+                insert_product()
+                opcionmenu=raw_input("To return to the menu y/n: ")
+                if opcionmenu.lower()=="y":
+                    salir=False
+                else:
+                    break
+            elif opmenu =="2":
 
-		        TOTALONE= compras()
-		        print TOTALONE
-		        opcionmenu=raw_input("Desea volver al menu SI/NO: ")
-		        if opcionmenu.lower()=="si":
-		            salir=False
-		        else:
-		            break
-		    elif opmenu =="3":
-		        print factura()
-		        opcionmenu=raw_input("Desea vover al menu SI/NO: ")
-		        if opcionmenu.lower()=="si":
-		            salir=False
-		        else:
-		            break
+                TOTALONE= shopping()
+                print TOTALONE
+                opcionmenu=raw_input("To return to the menu y/n: ")
+                if opcionmenu.lower()=="y":
+                    salir=False
+                else:
+                    break
+            elif opmenu =="3":
+                print BILL()
+                opcionmenu=raw_input("To return to the menu y/n: ")
+                if opcionmenu.lower()=="y":
+                    salir=False
+                else:
+                    break
+            elif opmenu == "4":
+                limpiar()
+
+                salir()
+            
     except:
-		print"Hello"
+        'enter a valid option'
